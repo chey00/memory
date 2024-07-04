7f6f88be5030e37636efbfab8a119e0e11c13e87
chey00 <115455604+chey00@users.noreply.github.com>
2024-07-04 08:32:00 +0200

    Added shuffel of pictures

--- CentralWidget.py
+++ CentralWidget.py
@@ -1,3 +1,5 @@
+import random
+
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QWidget, QGridLayout
@@ -9,10 +11,13 @@ class CentralWidget(QWidget):
     def __init__(self, parent=None):
         super(CentralWidget, self).__init__(parent)
 
+        list_of_front_covers = ["Audi.jpg", "BMW.jpg", "Mercedes.jpg", "Porsche.jpeg"] * 2
+        random.shuffle(list_of_front_covers)
+
         list_of_cards = list() # []
         number_of_cards = 8
         for i in range(number_of_cards):
-            list_of_cards.append(MyLabel("cover.jpg", "BMW.jpg", parent))
+            list_of_cards.append(MyLabel("cover.jpg", list_of_front_covers[i], parent))
 
         layout = QGridLayout()
 
