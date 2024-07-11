08f12c62060b7230584b421d27669a1b76af2c63
chey00 <115455604+chey00@users.noreply.github.com>
2024-07-11 08:17:04 +0200

    Added non-threaded version

--- CentralWidget.py
+++ CentralWidget.py
@@ -10,12 +10,12 @@ class CentralWidget(QWidget):
     def __init__(self, parent=None):
         super(CentralWidget, self).__init__(parent)
 
-        self.last_clicked_front_cover = None
+        self.last_sender = None
 
         list_of_front_covers = ["Audi.jpg", "BMW.jpg", "Mercedes.jpg", "Porsche.jpeg"] * 2
         random.shuffle(list_of_front_covers)
 
-        list_of_cards = list() # []
+        list_of_cards = list()
         number_of_cards = 8
         for i in range(number_of_cards):
             list_of_cards.append(MyLabel("cover.jpg", list_of_front_covers[i], parent))
@@ -30,14 +30,27 @@ class CentralWidget(QWidget):
 
         self.setLayout(layout)
 
-    @pyqtSlot(str)
-    def handle_cards(self, front_cover_name):
-        print(front_cover_name)
+    @pyqtSlot()
+    #  Erweitern Sie den Slot so, dass die Karte für eine Sekunde aufgedeckt bleibt.
+    def handle_cards(self):
+        self.sender().switch()
 
-        if self.last_clicked_front_cover:
-            if self.last_clicked_front_cover == front_cover_name:
-                print("Match!")
-                
-            self.last_clicked_front_cover = None
+        if self.last_sender:
+            if self.last_sender == self.sender():
+                return
+
+            elif self.last_sender.str_front() == self.sender().str_front():
+                self.sender().deactivate()
+                self.last_sender.deactivate()
+
+                self.sender().signal_front_cover_name.disconnect(self.handle_cards)
+                self.last_sender.signal_front_cover_name.disconnect(self.handle_cards)
+
+                self.last_sender = None
+            else:
+                self.sender().switch()
+                self.last_sender.switch()
+
+                self.last_sender = None
         else:
-            self.last_clicked_front_cover = front_cover_name
+            self.last_sender = self.sender()
--- MyLabel.py
+++ MyLabel.py
@@ -1,23 +1,29 @@
 from PyQt6.QtCore import Qt, pyqtSlot, pyqtSignal
 from PyQt6.QtGui import QPixmap
-from PyQt6.QtWidgets import QLabel
+from PyQt6.QtWidgets import QLabel, QStyle, QFrame, QStyleOptionFrame
 
 
 class MyLabel(QLabel):
-    signal_front_cover_name = pyqtSignal(str)
+    signal_front_cover_name = pyqtSignal()
 
     def __init__(self, back, front, parent=None):
         super(MyLabel, self).__init__(parent)
 
-        self.str_front = front
-        self.pixmap_front = QPixmap(front).scaledToWidth(250, Qt.TransformationMode.SmoothTransformation)
+        self.setFrameStyle(QFrame.Shape.Panel + QFrame.Shadow.Raised)
+        self.setLineWidth(3)
+
+        self.__str_front = front
+        self.pixmap_front = QPixmap(self.__str_front).scaledToWidth(250, Qt.TransformationMode.SmoothTransformation)
 
         self.show_back = False
         self.pixmap_back = QPixmap(back).scaledToWidth(250, Qt.TransformationMode.SmoothTransformation)
         self.setPixmap(self.pixmap_back)
 
+    def str_front(self):
+        return self.__str_front
+
     def mouseReleaseEvent(self, ev) -> None:
-        self.switch()
+        self.signal_front_cover_name.emit()
 
     @pyqtSlot()
     def switch(self):
@@ -25,7 +31,9 @@ class MyLabel(QLabel):
             self.setPixmap(self.pixmap_back)
             self.show_back = False
         else:
-            self.signal_front_cover_name.emit(self.str_front)
-
             self.setPixmap(self.pixmap_front)
             self.show_back = True
+
+    @pyqtSlot()
+    def deactivate(self):
+        self.setFrameStyle(QFrame.Shape.Panel + QFrame.Shadow.Sunken)
